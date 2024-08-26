d9e83687b58be826c358b97cf55eeab846428ec1
rawvie <rawvie@ngit.com.np>
2024-08-26 11:55:16 +0545

    init

---
+++ src/package.js
@@ -0,0 +1,8 @@
+export default {
+  "name": "@liqwid-labs/lucid",
+  "version": "0.11.3",
+  "license": "MIT",
+  "author": "Alessandro Konrad",
+  "description": "Lucid is a library, which allows you to create Cardano transactions and off-chain code for your Plutus contracts in JavaScript, Deno and Node.js.",
+  "repository": "https://github.com/liqwid-labs/lucid"
+};
